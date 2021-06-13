f989bc86fde32a9bda0c5cfad9aae59e2803190c
Yerzh030 <e.sultanbekov1@gmail.com>
2021-06-13 21:52:59 +0600

    15 done

--- 15_IDE/06_coin2/main.py
+++ 15_IDE/06_coin2/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+import math
+def check_r(x,y,r):
+    h = math.sqrt(x**2 + y**2)
+    if h > r:
+        print('Монетки рядом нет')
+    else:
+        print("Монетка где-то рядом")
+x = float(input("X:"))
+y = float(input("Y:"))
+r = float(input("r:"))
+check_r(x,y,r)
--- 15_IDE/07_years/main.py
+++ 15_IDE/07_years/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+def check_year(year):
+    numone = numtwo = year
+    count= 0
+    while numone != 0:
+        n1 = numone % 10
+        count  = 1
+        numone = numone // 10
+        numtwo = numone
+        while numtwo != 0:
+            n2 = numtwo %10
+            numtwo = numtwo //10
+            if n1 == n2:
+                count +=1
+            if count == 3:
+                return  True
+    return  False
+
+first_year = int(input("Введите первый год:"))
+second_year = int(input("Введите второй год:"))
+for i in range(first_year,second_year+1):
+    if check_year(i) == True:
+        print(i)
