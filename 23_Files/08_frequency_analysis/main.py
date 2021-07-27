f64df3bcb76a0789958ae5d70667c08a194a5fb1
kazakhcoder <e.sultanbekov1@gmail.com>
2021-07-27 09:10:35 +0600

    module 22

--- 23_Files/08_frequency_analysis/main.py
+++ 23_Files/08_frequency_analysis/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+def collect_stats(file_name):
+    result = {}
+    text = open(file_name,'r',encoding="utf-8")
+    sum = 0
+    for i_line  in text:
+        for j_char in i_line:
+            if j_char.lower() in 'qwertyuiopasdfghjklmnbvcxz' :
+                if j_char not in result:
+                    result[j_char] = 0
+                result[j_char] +=1
+                sum +=1
+    text.close()
+    for key in result.keys():
+        result[key] = result[key] / sum
+    return  result
+file_name = "text.txt"
+stats = collect_stats(file_name)
+for key,value in stats.items():
+    print(key," " ,value)
--- 23_Files/09_war_and_peace/main.py
+++ 23_Files/09_war_and_peace/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+def collect_stats(file_name):
+    result = {}
+    text = open(file_name,'r',encoding="utf-8")
+    for i_line  in text:
+        for j_char in i_line:
+            if j_char.isalpha():
+                if j_char not in result:
+                    result[j_char] = 0
+                result[j_char] +=1
+    text.close()
+    return  result
+file_name = "voyna-i-mir.txt"
+stats = collect_stats(file_name)
+print(stats)
