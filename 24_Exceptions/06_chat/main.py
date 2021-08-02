3da44000c6ee1a30bed9932a78baa2b6136354ba
kazakhcoder <e.sultanbekov1@gmail.com>
2021-08-02 11:16:26 +0600

    add  solution for 6 problem

--- 24_Exceptions/06_chat/main.py
+++ 24_Exceptions/06_chat/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+name = input("What is your name?")
+while True:
+    print("Input 1 to see chat text or 2  to write a message")
+    response = input("Your input:")
+    if response == "1":
+        try:
+            with open("chat.txt","r+") as file:
+                message  = file.readlines()
+                print(" ".join(message))
+        except FileNotFoundError:
+            print("File is not exitst")
+    elif response == '2':
+        new_message = input("input the message:")
+        with open("chat.txt",'a') as file:
+            file.write('{name}: {message}'.format(name = name, message= new_message))
+
